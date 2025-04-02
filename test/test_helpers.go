8aab5e29da6fd4cadc92cd9ecbddb2d84fe87e6f
artemwebber1 <chekhonin.artem@gmail.com>
2025-04-02 21:19:29 +0300

    Переписан путь к конфигурации

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -25,7 +25,7 @@ var mock = m{
 	pwd:   "password4321",
 }
 
-var cfg = config.NewConfig(`../config/config.json`)
+var cfg = config.NewConfig("../config/config.json")
 
 var addr = cfg.Host + ":" + cfg.Port + cfg.Prefix
 
