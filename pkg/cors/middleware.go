dc6daa834a76e9931f0aa10f15ef24d54d216b5e
artemwebber1 <chekhonin.artem@gmail.com>
2025-04-24 21:01:02 +0300

    Метод PUT заменён методом PATCH

--- pkg/cors/middleware.go
+++ pkg/cors/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func Middleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method != http.MethodOptions {
