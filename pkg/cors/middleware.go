6bf21d4e03c73f65875b8d059368ef39aa765ec7
artemwebber1 <chekhonin.artem@gmail.com>
2025-04-25 21:34:38 +0300

    обновлён cors

--- pkg/cors/middleware.go
+++ pkg/cors/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func Middleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
